5cc66f5d3920b10c6bbdc1cb6d19544c190132d7
Jenni Davila <pazjenni1331@gmail.com>
2021-06-16 20:23:16 -0500

    Created random button to display results onto modal.  Still needing to fix how displayed on modal

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,4 @@
-var dummyRecipeUrl = "https://api.spoonacular.com/recipes/complexSearch?query=pasta&apiKey=646a3c17f65f48eb8dc89229bd07fab4&addRecipeInformation=true&fillIngredients=true";
+var dummyRecipeUrl = "https://api.spoonacular.com/recipes/random?number=1&fab4&addRecipeInformation=true&fillIngredients=true&apiKey=f59f88cb8fbf4ac0b0c99e345526552a";
 var dummyMovieUrl = "https://omdbapi.com/?apikey=48a5261b&t=star%20wars";
 
 function getId(id){
@@ -18,12 +18,12 @@ function createElement(parent, element, childId){
 
 fetch(dummyRecipeUrl)
     .then(function(response){
+        console.log(response)
         return response.json();
     })
-    .then(function(data){
-        var recipe = data.results[1]
-        displayRecipe(recipe);
-    })
+    .then(data => displayRecipe(data)).catch(err => console.log(err))
+        // var recipe = data.recipes[0]
+        // console.log(data)
 
 fetch(dummyMovieUrl)
     .then(function(response){
@@ -33,16 +33,17 @@ fetch(dummyMovieUrl)
         displayMovie(data);
     })
 
-function displayRecipe(recipe){
-    console.log(recipe);
+function displayRecipe (data) {
+    console.log(data);
+
     createElement(yourRecipeInfo, "img", "foodImage")
-    getId("foodImage").setAttribute("src", recipe.image)
+    getId("foodImage").setAttribute("src", data.recipes[0].image)
     createElement(yourRecipeInfo, "h2", "recipeTitle")
-    getId("recipeTitle").textContent = recipe.title
+    getId("recipeTitle").textContent = data.recipes[0].title
     createElement(yourRecipeInfo, "p", "summary")
-    getId("summary").innerHTML = recipe.summary
+    getId("summary").innerHTML = data.recipes[0].summary
     createElement(yourRecipeInfo, "p", "timeRequired")
-    getId("timeRequired").textContent = "Ready in " + recipe.readyInMinutes + " minutes"
+    getId("timeRequired").textContent = "Ready in " + data.recipes[0].readyInMinutes + " minutes"
 }
 
 function displayMovie(data){
@@ -67,3 +68,19 @@ function displayMovie(data){
     createElement(movieStats, "li", "runtime")
     getId("runtime").textContent = "Runtime: " + data.Runtime
 }
+
+
+function randomEl(event) {
+    // event.preventDefault();
+
+    // var btnclick = target.event
+
+    document.querySelector(".results-modal-window").style.display = "block";
+    displayRecipe();
+    displayMovie();
+
+}
+
+
+
+document.getElementById("randomBtn").addEventListener("click", randomEl)
